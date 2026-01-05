8e9759e4fc0b6d0b84eca848cb565a61845113e0
Elvis T <elvisptavares@gmail.com>
2026-01-05 18:15:34 -0300

    feat: enhance account registration form with improved validation and UI

--- app/pages/Dashboard/Accounts/list.vue
+++ app/pages/Dashboard/Accounts/list.vue
@@ -11,73 +11,6 @@
             class="text-xl md:text-2xl font-bold mb-4"
             style="color: var(--color-primary)"
           >
-            Nova Conta
-          </h2>
-          <form @submit.prevent="saveAccount" class="flex flex-col gap-4">
-            <div class="flex flex-col gap-2">
-              <label class="font-semibold">Nome</label>
-              <UInput
-                v-model="state.name"
-                placeholder="Ex: Conta Corrente"
-                class="bg-gray-100"
-              />
-            </div>
-            <div class="flex flex-col gap-2">
-              <label class="font-semibold">Tipo</label>
-              <UInput
-                v-model="state.type"
-                placeholder="Ex: Bancária"
-                class="bg-gray-100"
-              />
-            </div>
-
-            <div class="flex flex-col gap-2">
-              <label class="font-semibold">Categoria</label>
-              <USelect
-                v-model="state.category"
-                :items="items"
-                class="bg-gray-100"
-              />
-            </div>
-
-            <div class="flex flex-col gap-2">
-              <label class="font-semibold">Valor</label>
-              <UInput
-                v-model="state.amount"
-                type="number"
-                placeholder="0.00"
-                class="bg-gray-100"
-              />
-            </div>
-
-            <div class="flex flex-col gap-2">
-              <label class="font-semibold">Mês e ano</label>
-              <div class="flex gap-2">
-                <USelect
-                  v-model="state.month"
-                  :items="monthOptions"
-                  placeholder="Mês"
-                  class="bg-gray-100 flex-1"
-                />
-                <USelect
-                  v-model="state.year"
-                  :items="yearOptions"
-                  placeholder="Ano"
-                  class="bg-gray-100 flex-1"
-                />
-              </div>
-            </div>
-            <UButton type="submit" class="w-fit font-bold rounded-full">
-              Salvar
-            </UButton>
-          </form>
-        </div>
-
-        <div class="p-4 md:p-8 rounded-lg shadow-md w-full">
-          <h2
-            class="text-xl md:text-2xl font-bold mb-4"
-            style="color: var(--color-primary)"
-          >
             Contas
           </h2>
           <div class="overflow-x-auto">
@@ -136,6 +69,13 @@
         </div>
       </div>
     </div>
+    <UButton
+      icon="i-heroicons-plus"
+      size="lg"
+      class="fixed top-4 right-4 z-10"
+      @click="$router.push('/dashboard/accounts/register')"
+      >Adicionar Despesa</UButton
+    >
   </UDashboardGroup>
 </template>
 
@@ -150,74 +90,74 @@ import { useDespesas } from "../../../../composables/useDespesas";
 import { useCategories } from "../../../../composables/useCategories";
 
 const { listarDespesas } = useDespesas();
-const { listCategories } = useCategories();
-const { criarDespesa } = useDespesas();
+// const { listCategories } = useCategories();
+// const { criarDespesa } = useDespesas();
 
 const despesas = ref([]);
 
-const categories = ref([]);
+// const categories = ref([]);
 
-const items = ref([
-  // { label: "Alimentação", value: 1 },
-  // { label: "Casa", value: 2 },
-]);
-const value = ref(1);
+// const items = ref([
+//   // { label: "Alimentação", value: 1 },
+//   // { label: "Casa", value: 2 },
+// ]);
+// const value = ref(1);
 
-const state = reactive({
-  name: undefined,
-  type: undefined,
-  amount: undefined,
-  status: "Ativa",
-  category: undefined,
-});
+// const state = reactive({
+//   name: undefined,
+//   type: undefined,
+//   amount: undefined,
+//   status: "Ativa",
+//   category: undefined,
+// });
 
-const monthOptions = [
-  { label: "Janeiro", value: 1 },
-  { label: "Fevereiro", value: 2 },
-  { label: "Março", value: 3 },
-  { label: "Abril", value: 4 },
-  { label: "Maio", value: 5 },
-  { label: "Junho", value: 6 },
-  { label: "Julho", value: 7 },
-  { label: "Agosto", value: 8 },
-  { label: "Setembro", value: 9 },
-  { label: "Outubro", value: 10 },
-  { label: "Novembro", value: 11 },
-  { label: "Dezembro", value: 12 },
-];
+// const monthOptions = [
+//   { label: "Janeiro", value: 1 },
+//   { label: "Fevereiro", value: 2 },
+//   { label: "Março", value: 3 },
+//   { label: "Abril", value: 4 },
+//   { label: "Maio", value: 5 },
+//   { label: "Junho", value: 6 },
+//   { label: "Julho", value: 7 },
+//   { label: "Agosto", value: 8 },
+//   { label: "Setembro", value: 9 },
+//   { label: "Outubro", value: 10 },
+//   { label: "Novembro", value: 11 },
+//   { label: "Dezembro", value: 12 },
+// ];
 
-const currentYear = new Date().getFullYear();
-const yearOptions = Array.from({ length: 11 }, (_, i) => ({
-  label: (currentYear + i).toString(),
-  value: currentYear + i,
-}));
+// const currentYear = new Date().getFullYear();
+// const yearOptions = Array.from({ length: 11 }, (_, i) => ({
+//   label: (currentYear + i).toString(),
+//   value: currentYear + i,
+// }));
 
 // const saveAccount = () => {
 //   console.log("Form Data:", state);
 // };
 
-async function saveAccount() {
-  // Lógica para salvar a conta
-  console.log("Salvando conta:", state);
-  await criarDespesa({
-    nome: state.name,
-    tipo: state.type,
-    valor: state.amount,
-    id_category: state.category,
-    month: state.month,
-    year: state.year,
-  });
-}
+// async function saveAccount() {
+//   // Lógica para salvar a conta
+//   console.log("Salvando conta:", state);
+//   await criarDespesa({
+//     nome: state.name,
+//     tipo: state.type,
+//     valor: state.amount,
+//     id_category: state.category,
+//     month: state.month,
+//     year: state.year,
+//   });
+// }
 
 onMounted(async () => {
   try {
     despesas.value = await listarDespesas();
-    categories.value = await listCategories();
+    // categories.value = await listCategories();
 
-    items.value = categories.value.map((category) => ({
-      label: category.name,
-      value: category.id,
-    }));
+    // items.value = categories.value.map((category) => ({
+    //   label: category.name,
+    //   value: category.id,
+    // }));
   } catch (error) {
     console.error("Erro ao buscar despesas:", error);
   }
